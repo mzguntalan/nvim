775367b8c269818cc9f315ca4b8c0bd75e38cf93
mzguntalan <mzguntalan@gmail.com>
2024-06-28 09:35:56 +0900

    References and renaming

--- lua/config/plugin/lsp.lua
+++ lua/config/plugin/lsp.lua
@@ -9,6 +9,8 @@ local custom_attach = function(client)
     vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
     vim.keymap.set('n','gs','<cmd>lua vim.lsp.buf.hover()<CR>')
+    vim.keymap.set('n', '<leader>rf', '<cmd>lua vim.lsp.buf.references()<CR>')
+    vim.keymap.set('n', '<leader>re', '<cmd>lua vim.lsp.buf.rename()<CR>')
     vim.keymap.set('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
 end
 
