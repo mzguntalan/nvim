7dee170c976008c413737171c732f9b46eb929d0
mzguntalan <mzguntalan@gmail.com>
2024-11-05 10:36:21 +0900

    Change illuminate color

--- lua/config/plugin/colorscheme.lua
+++ lua/config/plugin/colorscheme.lua
@@ -8,37 +8,13 @@ tokyonight.setup({
     transparent = false,
     on_colors = function(colors) 
         colors.bg = '#ffffff'
+        colors.bg_float = '#ffffff'
+        colors.bg_visual = '#edf8ff'
+        colors.bg_dark = '#edf8ff'
     end,
-    on_highlights = function(hl, c)
-        local prompt = "#2d3149"
-    -- hl.TelescopeNormal = {
-    --   bg = c.bg_light,
-    --   fg = c.bg_light,
-    -- }
-    -- hl.TelescopeBorder = {
-    --   bg = c.bg_dark,
-    --   fg = c.bg_dark,
-    -- }
-    -- hl.TelescopePromptNormal = {
-    --   bg = prompt,
-    -- }
-    -- hl.TelescopePromptBorder = {
-    --   bg = prompt,
-    --   fg = prompt,
-    -- }
-    -- hl.TelescopePromptTitle = {
-    --   bg = prompt,
-    --   fg = prompt,
-    -- }
-    -- hl.TelescopePreviewTitle = {
-    --   bg = c.bg_dark,
-    --   fg = c.bg_dark,
-    -- }
-    -- hl.TelescopeResultsTitle = {
-    --   bg = c.bg_dark,
-    --   fg = c.bg_dark,
-    -- }
-  end,
+    on_highlights = function(highlights, colors)
+
+    end,
 })
 
 vim.cmd("colorscheme tokyonight")
