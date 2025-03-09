3cf73db43ca1c93f0c363fc727ce975888040f2e
mzguntalan <mzguntalan@gmail.com>
2025-03-09 15:00:49 +0900

    Added elixir support

--- lua/zolo/packer.lua
+++ lua/zolo/packer.lua
@@ -86,4 +86,5 @@ return require('packer').startup(function(use)
   use('RRethy/vim-illuminate')
   use{'supabase-community/postgres_lsp'}
   use('mrcjkb/haskell-tools.nvim')
+  use({ "elixir-tools/elixir-tools.nvim", tag = "stable", requires = { "nvim-lua/plenary.nvim" }})
 end)
