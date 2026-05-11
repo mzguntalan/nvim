ee1ce2d5c69af15307d02942f26230e6464a49da
Tung Tran <tungts1101@gmail.com>
2026-05-11 12:13:59 +0900

    updated

--- lua/zolo/packer.lua
+++ lua/zolo/packer.lua
@@ -24,7 +24,7 @@ return require('packer').startup(function(use)
   --  end
   -- })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate', branch = "master"})
   use('mbbill/undotree')
   -- language
   use('williamboman/mason.nvim')
